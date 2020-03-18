42dc13bde9621b9b67b1fdc53b7fffd32426ea27
Kamil <ishmuratoof@icloud.com>
2020-03-18 11:43:45 +0500

    Initial Commit

---
+++ TimerApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TimerApp
+//
+//  Created by Камиль on 18.03.2020.
+//  Copyright © 2020 Kamil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TimerApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by Камиль on 18.03.2020.
+//  Copyright © 2020 Kamil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
